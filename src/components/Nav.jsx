dc42e760637ce822069260ec00e490987b5d9526
cps97 <colinstoutt@gmail.com>
2023-05-03 23:09:44 -0700

    add league id from local storage tonav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-export const Nav = () => {
-  return <div></div>;
+const Nav = () => {
+  let league_id;
+  if (typeof window !== "undefined") {
+    league_id = localStorage.getItem("league_id");
+  }
+
+  return (
+    <nav>
+      <Link
+        to="/league-id"
+        className="fixed top-5 left-5 px-6 py-2 text-sleeperGrey hover:text-teal uppercase font-semibold"
+      >
+        <h1 className="font-light">
+          <span className="font-bold">League ID:</span> {league_id}
+        </h1>
+      </Link>
+    </nav>
+  );
 };
+
+export default Nav;
