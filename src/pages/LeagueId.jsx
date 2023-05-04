d2982c26783c1cdc975f3e83e4c4a32cd5ee60e8
cps97 <colinstoutt@gmail.com>
2023-05-03 22:00:36 -0700

    solve tailwind issues, add leagueId page

--- src/pages/LeagueId.jsx
+++ src/pages/LeagueId.jsx
@@ -1,5 +1,58 @@
-import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const LeagueId = () => {
-  return <div>LeagueId</div>;
+  const [leagueId, setLeagueId] = useState("");
+  const [notValid, setNotValid] = useState(false);
+  const [toggleLearnMore, setToggleLearnMore] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setLeagueId(e.target.value);
+    console.log(leagueId);
+  };
+  const handleSubmit = () => {
+    if (leagueId.length === 18) {
+      localStorage.setItem("league_id", leagueId);
+      navigate("/");
+    } else {
+      setNotValid(true);
+    }
+  };
+
+  return (
+    <main className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-3/4 max-w-2xl  p-8 min-w-full text-center rounded-lg  font-semibold">
+      <h1 className="text-2xl mb-4 text-white">
+        To start, please enter your league ID in the field below.
+      </h1>
+
+      <input
+        type="number"
+        value={leagueId}
+        onChange={handleChange}
+        className="leagueId mb-2 rounded-tl-md rounded-bl-md pl-4 pr-2 py-1 "
+      />
+      <button
+        type="submit"
+        onClick={handleSubmit}
+        className=" bg-teal px-4 py-1 
+        lg:rounded-tr-md lg:rounded-br-md
+        rounded-tr-md rounded-br-md active:bg-darkTeal active:text-teal"
+      >
+        Start
+      </button>
+      {notValid ? (
+        <h6 className="text-red font-normal">Please enter a valid league ID</h6>
+      ) : (
+        <></>
+      )}
+      <h6 className="text-sleeperGrey font-normal">
+        Dont know league ID?{" "}
+        <span className="underline hover:text-teal active:text-darkTeal cursor-pointer">
+          Learn more
+        </span>
+      </h6>
+    </main>
+  );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,50 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      darkBg: "#181f2e",
+      lightBg: "#2e3748",
+      borderColor: "#637087",
+      sleeperGrey: "#9cb5cc",
+      teal: "#00ceb8",
+      darkTeal: "#284e57",
+      btnText: "#022046",
+      red: "#ff0000",
+    },
+    screens: {
+      xs: "1px",
+
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      xxl: "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      width: {
+        340: "340px",
+      },
+    },
   },
   plugins: [],
-}
-
+};
