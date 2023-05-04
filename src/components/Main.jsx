ac9b5ce00fa1df3b193780ee33eb88abf0147d83
cps97 <colinstoutt@gmail.com>
2023-05-03 23:09:19 -0700

    add routes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import { Index } from "../pages/Index";
 import { LeagueId } from "../pages/LeagueId";
@@ -8,14 +8,12 @@ import { LotteryDraft } from "../pages/LotteryDraft";
 export const Main = () => {
   return (
     <main>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />}></Route>
-          <Route path="/league-id" element={<LeagueId />}></Route>
-          <Route path="/draft" element={<Draft />}></Route>
-          <Route path="/lottery-draft" element={<LotteryDraft />}></Route>
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />}></Route>
+        <Route path="/league-id" element={<LeagueId />}></Route>
+        <Route path="/draft" element={<Draft />}></Route>
+        <Route path="/lottery-draft" element={<LotteryDraft />}></Route>
+      </Routes>
     </main>
   );
 };
