771d9368fa995ee19b9844fd97b929ec672be4e3
cps97 <colinstoutt@gmail.com>
2023-05-04 12:18:08 -0700

    make mobile responsive

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,70 +1,65 @@
-import { Link } from "react-router-dom";
-import SportsFootballIcon from "@mui/icons-material/SportsFootball";
-import PercentIcon from "@mui/icons-material/Percent";
-import Nav from "../components/Nav";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const Index = () => {
+  const [leagueId, setLeagueId] = useState("");
+  const [notValid, setNotValid] = useState(false);
+  const [toggleLearnMore, setToggleLearnMore] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setLeagueId(e.target.value);
+    console.log(leagueId);
+  };
+  const handleSubmit = () => {
+    if (leagueId.length === 18) {
+      localStorage.setItem("league_id", leagueId);
+      navigate("/home");
+    } else {
+      setNotValid(true);
+    }
+  };
+
   return (
-    <main>
-      <Nav />
-      <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white">
-        <div className="flex sm:flex-nowrap flex-wrap sm:gap-x-10 gap-y-5">
-          <Link
-            to="/draft"
-            className="transition-all duration-200 sm:h-96 sm:w-72 h-52 w-72 bg-lightBg relative hover:text-teal active:text-teal rounded"
-          >
-            <h1 className=" text-4xl font-semibold px-4 py-4">
-              Generate
-              <br />
-              Draft
-              <br />
-              Order
-            </h1>
-            <SportsFootballIcon
-              className="text-darkBg"
-              sx={{
-                fontSize: "10rem",
-                position: "absolute",
-                bottom: "0",
-                right: "0",
-                opacity: "0.3",
-              }}
-            />
-          </Link>
-          <Link
-            to="/lotteryDraft"
-            className="transition-all duration-200 sm:h-96 sm:w-72 h-52 w-72  bg-lightBg relative hover:text-teal active:text-teal rounded"
-          >
-            <h1 className="text-4xl font-semibold px-4 py-4">
-              Generate
-              <br />
-              Lottery
-              <br />
-              Draft
-              <br />
-              Order
-            </h1>
-            <SportsFootballIcon
-              className="text-darkBg text-lg"
-              sx={{
-                fontSize: "10rem",
-                position: "absolute",
-                bottom: "0",
-                right: "0",
-                opacity: "0.3",
-              }}
-            />
-            <PercentIcon
-              className="text-lightBg"
-              sx={{
-                fontSize: "3.8rem",
-                position: "absolute",
-                bottom: "3.1rem",
-                right: "3.1rem",
-              }}
-            />
-          </Link>
-        </div>
+    <main className="overflow-y-hidden">
+      <h1 className="mt-24 text-4xl font-semibold text-white text-center">
+        Sleeper League Draft Generator
+      </h1>
+      <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-3/4 max-w-2xl  p-8 min-w-full text-center rounded-lg  font-semibold">
+        <h1 className="text-2xl mb-5 text-white">
+          To start, please enter your Sleeper league ID below.
+        </h1>
+
+        <input
+          type="number"
+          value={leagueId}
+          onChange={handleChange}
+          className="leagueId sm:rounded-tl-md sm:rounded-bl-md sm:rounded-tr-none sm:rounded-br-none rounded pl-4 pr-2 py-1 "
+        />
+        <br className="sm:hidden" />
+        <button
+          type="submit"
+          onClick={handleSubmit}
+          className=" bg-teal px-4 py-1 sm:mb-2 mt-2 mb-4
+        sm:rounded-tr-md sm:rounded-br-md sm:rounded-bl-none sm:rounded-tl-none
+        rounded-lg active:bg-darkTeal active:text-teal"
+        >
+          Start
+        </button>
+        {notValid ? (
+          <h6 className="text-red font-normal">
+            Please enter a valid league ID
+          </h6>
+        ) : (
+          <></>
+        )}
+        <h6 className="text-sleeperGrey font-normal">
+          Dont know league ID?{" "}
+          <span className="underline hover:text-teal active:text-darkTeal cursor-pointer">
+            Click here
+          </span>
+        </h6>
       </div>
     </main>
   );
